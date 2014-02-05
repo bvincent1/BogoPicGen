43ee77087cfab1500ed523309f586f0606226cc0
bvincent1 <vincent@ualberta.ca>
2014-02-04 18:58:20 -0700

    Scaled image to screen size

--- src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
+++ src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
@@ -35,26 +35,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import es.softwareprocess.bogopicgen.*;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.view.View.OnClickListener;
+
 public class BogoPicGenActivity extends Activity {
 
 	Uri imageFileUri;
